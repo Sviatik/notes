82a0d410036fd669ceb24bfd7742af79968ff048
Sviatoslav Stetsiak <sstetsyak@gmail.com>
2018-01-30 16:32:20 +0200

    add java to logstash script

--- aws-init-scripts/elk/logstash.userdata.sh
+++ aws-init-scripts/elk/logstash.userdata.sh
@@ -13,6 +13,11 @@ echo "INFO: START INSTAL PACKAGES"
 yum install -y epel-release
 yum install -y wget curl vim telnet bash-completion
 
+echo "INFO: START INSTAL JAVA"
+cd /tmp
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.rpm"
+sudo yum -y localinstall jdk-8u161-linux-x64.rpm
+
 sudo rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
 
 
