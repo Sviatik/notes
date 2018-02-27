4135ada90ad9f8b4c7ba4da2ddde6e574b848435
sviat <sstetsyak@gamil.com>
2018-02-27 23:02:26 +0200

    update

---
+++ aws-init-scripts/openvpn/openvpn_init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+echo "############################################"
+echo "###########START BOOTSTRAP SCRIPT###########"
+echo "############################################"
+
+#create user and add pub keys
+useradd -m sviatik -g admin
+mkdir /home/sviatik/.ssh
+curl https://github.com/sviatik.keys >> /home/sviatik/.ssh/authorized_keys 
+
+# Disable SELinux
+setenforce 0
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+
+yum upgrade
+echo "INFO: START INSTAL PACKAGES"
+yum install -y epel-release
+yum install -y wget curl vim telnet bash-completion tmux openvpn
+
+
+
+
+echo "############################################"
+echo "############END BOOTSTRAP SCRIPT############"
+echo "############################################"
